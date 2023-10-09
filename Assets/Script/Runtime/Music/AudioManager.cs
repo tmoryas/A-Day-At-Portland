23bb7e5c744e90fe3209cd993de3fd295899d938
Artyu <arthurpejoiane@hotmail.fr>
2023-10-10 00:32:54 +0200

    [ADD] A lot of packages

---
+++ Assets/Script/Runtime/Music/AudioManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using Random = UnityEngine.Random;
+using Unity.VisualScripting;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    
+    private Dictionary<SoundState, List<Sound>> DicoActualSound = new Dictionary<SoundState, List<Sound>>();
+    
+    void Awake()
+    {
+       foreach (Sound s in sounds)
+       {
+            s.Source = gameObject.AddComponent<AudioSource>();
+            s.Source.clip = s.Clip;
+
+            s.Source.volume = s.Volume;
+            s.Source.pitch = s.Pitch;
+            s.Source.loop = s.Loop;
+            s.Source.outputAudioMixerGroup = s.AudioMixer;
+
+            if (!DicoActualSound.ContainsKey(s.ActualSound))
+            {
+                DicoActualSound.Add(s.ActualSound, new List<Sound>());
+            }
+            DicoActualSound[s.ActualSound].Add(s);
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.Name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+        s.Source.Play();
+    }
+
+    public void Stop(SoundState soundState)
+    {
+        if (DicoActualSound.ContainsKey(soundState))
+        {
+            int i = Random.Range(0, DicoActualSound[soundState].Count);
+
+            Sound s = DicoActualSound[soundState][i];
+            if (s == null)
+            {
+                Debug.LogWarning("Sound: " + name + " not found");
+                return;
+            }
+            s.Source.Stop();
+
+        }
+        else
+        {
+            throw new ArgumentNullException();
+        }
+    }
+
+    public void PlayRandom(SoundState soundState)
+    {
+        if (DicoActualSound.ContainsKey(soundState))
+        {
+            int i = Random.Range(0, DicoActualSound[soundState].Count);
+
+            Sound s = DicoActualSound[soundState][i];
+            if (s == null)
+            {
+                Debug.LogWarning("Sound: " + name + " not found");
+                return;
+            }
+            s.Source.Play();
+        }
+        else
+        {
+            throw new ArgumentNullException();
+        }   
+    }
+}
---
+++ Assets/Script/Runtime/Music/Sound.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    [SerializeField] private string name;
+    [SerializeField] private AudioClip clip;
+    [SerializeField, Range(0f, 1f)] private float volume = 0.9f;
+    [SerializeField, Range(.1f, 3f)] private float pitch = 1f;
+    [SerializeField] private bool loop;
+    
+    [SerializeField] private AudioMixerGroup audioMixer;
+
+    private AudioSource source;
+    [SerializeField] private SoundState actualSound;
+
+    #region PROPERTY
+    public string Name => name;
+    public AudioClip Clip => clip;
+    public float Volume => volume;
+    public float Pitch => pitch;
+    public bool Loop => loop;
+    public AudioMixerGroup AudioMixer => audioMixer;
+
+    public AudioSource Source { get => source; set => source = value; }
+    public SoundState ActualSound => actualSound;
+    #endregion
+}

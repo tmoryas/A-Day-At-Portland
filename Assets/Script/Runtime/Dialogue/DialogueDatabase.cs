46198becb655a6d9f696258f6c4e89932dad47a0
Artyu <arthurpejoiane@hotmail.fr>
2023-10-12 21:42:51 +0200

    [ADD] GameManager + [ADD] Zenject Installer Script

--- Assets/Script/Runtime/Dialogue/DialogueDatabase.cs
+++ Assets/Script/Runtime/Dialogue/DialogueDatabase.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class DialogueDatabase : ScriptableObject
 {
-    private List<DialogueData> dialogueDataList = new List<DialogueData>();
-
+    [SerializeField] private List<DialogueData> dialogueDataList = new List<DialogueData>();
     public List<DialogueData> DialogueDataList { get => dialogueDataList; set => dialogueDataList = value; }
+
+
+    [SerializeField] private Queue<DialogueData> dialogueQueue = new Queue<DialogueData>();
+    public Queue<DialogueData> DialogueQueue { get => dialogueQueue; set => dialogueQueue = value; }
 }

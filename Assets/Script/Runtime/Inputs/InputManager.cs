0cb482e73c6147278b0a7b02819238e54613d4e0
tmoryas <56356128+tmoryas@users.noreply.github.com>
2023-10-12 11:58:57 +0200

    [UP] Added maxPort

--- Assets/Script/Runtime/Inputs/InputManager.cs
+++ Assets/Script/Runtime/Inputs/InputManager.cs
@@ -7,16 +7,17 @@ using UnityEngine;
 public class InputManager : MonoBehaviour, IInputManager
 {
 
-    [SerializeField] private InputMap currentMap;
+    [SerializeField] private InputMap _currentMap;
     private Dictionary<KeyCode, int> portToCharaID = new Dictionary<KeyCode, int>();
     private List<int> portToCharaIDs;
+    [SerializeField] private int _maxPorts = 4;
 
     public List<int> PortToCharaIDs => portToCharaIDs;
 
     private void Start()
     {
         int i = 0;
-        foreach (KeyCode k in currentMap.PortKeys)
+        foreach (KeyCode k in _currentMap.PortKeys)
         {
             portToCharaID.Add(k, i++);
         }
@@ -25,16 +26,20 @@ public class InputManager : MonoBehaviour, IInputManager
     private void Update()
     {
         portToCharaIDs = GetActiveChara();
-        //foreach (int i in portToCharaIDs) UnityEngine.Debug.Log(i);
+        foreach (int i in portToCharaIDs) UnityEngine.Debug.Log(i);
     }
 
     private List<int> GetActiveChara()
     {
         List <int> tempList = new List<int>();
 
-        foreach (KeyCode k in currentMap.PortKeys)
+        foreach (KeyCode k in _currentMap.PortKeys)
         {
-            if (Input.GetKey(k)) tempList.Add(portToCharaID[k]);
+            if (Input.GetKey(k))
+            {
+                if (tempList.Count > _maxPorts) return tempList;
+                tempList.Add(portToCharaID[k]);
+            }
         }
 
         return tempList;

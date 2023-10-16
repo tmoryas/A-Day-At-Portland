a91a03fea11b49177c3edf0a29a9e4cbcdbe1a53
Artyu <arthurpejoiane@hotmail.fr>
2023-10-16 10:45:24 +0200

    [ADD] Sprite Gestion for frame and light effect

---
+++ Assets/Script/Runtime/Character/CharacterObj.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterObj : MonoBehaviour
+{
+    [SerializeField] private Image _background;
+    [SerializeField] private Image _character;
+    [SerializeField] private Image _frame;
+    [SerializeField] private Image _frame_shadow;
+
+    [SerializeField] private Image _lightEffect;
+
+    public Image Background => _background;
+    public Image Character => _character;
+    public Image Frame => _frame; 
+    public Image Frame_shadow => _frame_shadow;
+    public Image LightEffect => _lightEffect;
+}
